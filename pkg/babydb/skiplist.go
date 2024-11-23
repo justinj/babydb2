4b786f5d826a71058c81ecf00f2a15eb74771e96
Justin Jaffray <justin.jaffray@gmail.com>
2024-11-23 11:00:24 -0500

    done stuff

--- pkg/babydb/skiplist.go
+++ pkg/babydb/skiplist.go
@@ -1,116 +1,214 @@
 package babydb
 
 import (
+	"bytes"
 	"math/rand"
+	"sync/atomic"
+	"unsafe"
 )
 
 const sklMaxHeight = 12
-const sklCapacity = 1 << 16
+const sklCapacity = 1 << 32
 
 type node struct {
 	row   Row
-	stack []*node
+	stack []unsafe.Pointer
 }
 
 type SkiplistIterator struct {
 	sk   *Skiplist
-	node *node
+	node unsafe.Pointer
+}
+
+func (it *SkiplistIterator) Seek(key []byte) {
+	// We'll position at the first row with the given key.
+	rowKey := Row{
+		Key:     key,
+		Version: 0,
+	}
+
+	pred := &it.sk.nodes[0]
+	succ := (*node)(atomic.LoadPointer(&pred.stack[sklMaxHeight-1]))
+
+	for i := sklMaxHeight - 1; i >= 0; i-- {
+		for succ != nil && succ.row.lt(rowKey) {
+			pred = succ
+			succ = (*node)(atomic.LoadPointer(&pred.stack[i]))
+		}
+		if i > 0 {
+			succ = (*node)(atomic.LoadPointer(&pred.stack[i-1]))
+		}
+	}
+	it.node = unsafe.Pointer(pred)
+	it.Next()
 }
 
 func (it *SkiplistIterator) Next() (Row, bool) {
 	if it.node == nil {
 		return Row{}, false
 	}
-	row := it.node.row
-	it.node = it.node.stack[0]
+	node := (*node)(atomic.LoadPointer(&it.node))
+	row := node.row
+	it.node = node.stack[0]
 	return row, true
 }
 
 type Skiplist struct {
-	heap []byte
-	ptrs []*node
-	// TODO: this should be atomic?
-	heapPtr int
-	nodes   []node
+	heap    []byte
+	heapPtr atomic.Int64
+
+	// These are pointers to *node
+	ptrs    []unsafe.Pointer
+	ptrsPtr atomic.Int64
+
+	nodes    []node
+	nodesPtr atomic.Int64
 }
 
 func (sk *Skiplist) Iter() *SkiplistIterator {
 	return &SkiplistIterator{
+		sk:   sk,
 		node: sk.nodes[0].stack[0],
 	}
 }
 
+func (sk *Skiplist) Find(key []byte) ([]byte, bool) {
+	iter := sk.Iter()
+	iter.Seek(key)
+	row, ok := iter.Next()
+	if !ok {
+		return nil, false
+	}
+	if bytes.Equal(row.Key, key) {
+		return row.Value, true
+	}
+	return nil, false
+}
+
 func (sk *Skiplist) Insert(key []byte, value []byte, version uint64) bool {
-	if sk.heapPtr+len(key)+len(value) > len(sk.heap) {
+	bump := len(key) + len(value)
+	bumped := sk.heapPtr.Add(int64(bump))
+	if bumped > sklCapacity {
 		// Does not fit, this Skiplist should be kicked out and a new one should be allocated.
 		return false
 	}
-	keyLoc := sk.heapPtr
-	valueLoc := sk.heapPtr + len(key)
+
+	// Now we own the space, we can write the key and value.
+	keyLoc := bumped - int64(len(key)) - int64(len(value))
+	valueLoc := bumped - int64(len(value))
 	copy(sk.heap[keyLoc:], key)
 	copy(sk.heap[valueLoc:], value)
-	sk.heapPtr += len(key) + len(value)
 
+	// height := 1 + bits.LeadingZeros64(uint64(rand.Uint64()))
 	height := 1
-	for rand.Intn(2) == 0 && height < sklMaxHeight {
-		height++
+	for rand.Float64() < P && height < sklMaxHeight {
+		height += 1
 	}
 
-	if len(sk.nodes) == cap(sk.nodes) {
+	bumpedNodesPtr := int(sk.nodesPtr.Add(1))
+
+	if bumpedNodesPtr >= len(sk.nodes) {
 		// No more space for nodes, this Skiplist should be kicked out and a new one should be allocated.
 		return false
 	}
 
-	if len(sk.ptrs)+height > cap(sk.ptrs) {
+	bumpedPtrsPtr := int(sk.ptrsPtr.Add(int64(height)))
+
+	if bumpedPtrsPtr > len(sk.ptrs) {
 		// No more space for pointers, this Skiplist should be kicked out and a new one should be allocated.
 		return false
 	}
 
-	ptrsLoc := len(sk.ptrs)
-	sk.ptrs = append(sk.ptrs, make([]*node, height)...)
-
 	row := Row{
-		Key:     sk.heap[keyLoc : keyLoc+len(key)],
-		Value:   sk.heap[valueLoc : valueLoc+len(value)],
+		Key:     sk.heap[keyLoc : int(keyLoc)+len(key)],
+		Value:   sk.heap[valueLoc : int(valueLoc)+len(value)],
 		Version: version,
 	}
 
-	sk.nodes = append(sk.nodes, node{
+	ptrsLoc := bumpedPtrsPtr - height
+
+	sk.nodes[bumpedNodesPtr] = node{
 		row:   row,
 		stack: sk.ptrs[ptrsLoc : ptrsLoc+height],
-	})
-
-	for i := 0; i < height; i++ {
-		// Find the place to insert the new node
-		prev := &sk.nodes[0]
-		current := prev.stack[i]
-		for current != nil && current.row.lt(row) {
-			prev = current
-			current = current.stack[i]
-		}
-		// First, make the new node point to `current`
-		sk.nodes[len(sk.nodes)-1].stack[i] = current
-		// Then, make `prev` point to the new node
-		prev.stack[i] = &sk.nodes[len(sk.nodes)-1]
 	}
 
-	// node := &sk.nodes[len(sk.nodes)-1]
+	newNodePtr := unsafe.Pointer(&sk.nodes[bumpedNodesPtr])
+	newNode := (*node)(newNodePtr)
+
+	preds, succs := sk.findTowers(row.Key, row.Version)
+
+	for i := 0; i < height; i++ {
+		for {
+			// predPtr := unsafe.Pointer(&preds[i])
+			pred := (*node)(preds[i])
+
+			succPtr := atomic.LoadPointer(&succs[i])
+			// succ := (*node)(succPtr)
+
+			newNode.stack[i] = succPtr
+
+			// Then, make `prev` point to the new node
+			if atomic.CompareAndSwapPointer(&pred.stack[i], succPtr, newNodePtr) {
+				break
+			}
+
+			// We failed, so we need to recompute the towers.
+			preds, succs = sk.findTowers(row.Key, row.Version)
+			// Keep trying until we succeed
+		}
+	}
 
 	return true
 }
 
+// findTowers ...
+func (sk *Skiplist) findTowers(key []byte, version uint64) ([]unsafe.Pointer, []unsafe.Pointer) {
+	rowKey := Row{
+		Key:     key,
+		Version: version,
+	}
+	// TODO: we should not need to allocate these slices.
+	preds := make([]unsafe.Pointer, sklMaxHeight)
+	succs := make([]unsafe.Pointer, sklMaxHeight)
+
+	pred := &sk.nodes[0]
+	succ := (*node)(atomic.LoadPointer(&pred.stack[sklMaxHeight-1]))
+	for i := sklMaxHeight - 1; i >= 0; i-- {
+		for succ != nil && succ.row.lt(rowKey) {
+			pred = succ
+			succ = (*node)(atomic.LoadPointer(&pred.stack[i]))
+		}
+		preds[i] = unsafe.Pointer(pred)
+		succs[i] = unsafe.Pointer(succ)
+		if i > 0 {
+			succ = (*node)(atomic.LoadPointer(&pred.stack[i-1]))
+		}
+	}
+
+	return preds, succs
+}
+
+var P = 0.5
+
 func NewSkiplist() *Skiplist {
 	head := node{
 		// TODO: should this be in the heap?
-		stack: make([]*node, sklMaxHeight),
+		stack: make([]unsafe.Pointer, sklMaxHeight),
 	}
 
-	nodes := make([]node, 0, sklCapacity)
-	nodes = append(nodes, head)
+	// how to size this?
+	nodes := make([]node, 1<<16)
+	nodes[0] = head
 
-	return &Skiplist{
-		heap:  make([]byte, sklCapacity),
-		nodes: nodes,
-		ptrs:  make([]*node, 0, 1<<10),
+	sk := &Skiplist{
+		heap:     make([]byte, sklCapacity),
+		nodes:    nodes,
+		nodesPtr: atomic.Int64{},
+		ptrs:     make([]unsafe.Pointer, 1<<16),
+		ptrsPtr:  atomic.Int64{},
 	}
+	sk.nodesPtr.Store(1)
+	sk.ptrsPtr.Store(0)
+
+	return sk
 }
--- pkg/babydb/skiplist_test.go
+++ pkg/babydb/skiplist_test.go
@@ -2,28 +2,83 @@ package babydb
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
+	"sync"
+	"sync/atomic"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSkiplist(t *testing.T) {
 	sk := NewSkiplist()
 
-	for i := 0; i < 3; i++ {
-		if !sk.Insert([]byte(fmt.Sprintf("foo%d", i)), []byte(fmt.Sprintf("bar%d", i)), 1) {
-			t.Fatal("insert failed")
-		}
-	}
+	threads := 8
+	batchSize := 1000
 
-	n := 100
+	var data []Row
+	for i := 0; i < batchSize*threads; i++ {
+		data = append(data, Row{Key: []byte(fmt.Sprintf("foo%d", i)), Value: []byte(fmt.Sprintf("bar%d", i)), Version: 1})
+	}
+	rand.Shuffle(len(data), func(i, j int) {
+		data[i], data[j] = data[j], data[i]
+	})
+
+	var tooBig atomic.Bool
+
+	var wg sync.WaitGroup
+	for i := 0; i < threads; i++ {
+		wg.Add(1)
+		data := data[i*batchSize : (i+1)*batchSize]
+		go func() {
+			for _, row := range data {
+				if !sk.Insert(row.Key, row.Value, row.Version) {
+					tooBig.Store(true)
+					break
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	expected := data
+	sort.Slice(expected, func(i, j int) bool {
+		return expected[i].lt(expected[j])
+	})
+
+	actual := []Row{}
+
 	it := sk.Iter()
 	for row, ok := it.Next(); ok; row, ok = it.Next() {
-		spew.Dump(row)
-		n--
-		if n == 0 {
-			break
-		}
+		actual = append(actual, row)
+	}
 
+	if tooBig.Load() {
+		t.Fatal("too many elements")
+	}
+	assert.ElementsMatch(t, expected, actual)
+
+	// Test Find.
+
+	for _, row := range data {
+		v, ok := sk.Find(row.Key)
+		assert.True(t, ok)
+		assert.Equal(t, row.Value, v)
+	}
+
+}
+
+func TestSkiplistSize(t *testing.T) {
+	rows := make([]Row, 1000)
+	for i := range rows {
+		rows[i] = Row{Key: []byte(fmt.Sprintf("foo%d", i)), Value: []byte(fmt.Sprintf("bar%d", i)), Version: 1}
+	}
+
+	P = 0.5
+	sk := NewSkiplist()
+	for _, row := range rows {
+		sk.Insert(row.Key, row.Value, row.Version)
 	}
 }
